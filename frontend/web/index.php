ad20285597062af43f7a39ef271d533023925024
naxocaballero <naxocaballero@gmail.com>
2024-05-19 14:58:43 +0200

    Prueba de interacción con github (no me lo tengáis en cuenta)

---
+++ frontend/web/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<?php
+ini_set("display_errors", E_ALL);
+?>
+<html lang="es">
+
+<head>
+    <title>
+        PoS - Clovr Labs
+    </title>
+
+    <base href="./">
+
+    <link rel="stylesheet" type="text/css" href="./css/root.css">
+    <link rel="stylesheet" type="text/css" href="./css/colors.css">
+    <link rel="stylesheet" type="text/css" href="/css/header.css">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+    <link rel="stylesheet" type="text/css" href="./css/styles_media.css">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="./img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon/favicon-16x16.png">
+    <link rel="mask-icon" href="./img/favicon/apple-touch-icon.png" color="#ffffff">
+    <link rel="shortcut icon" href="./img/favicon/favicon.ico">
+
+    <!-- Viewport y Apple -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="theme-color" content="#7b1af7">
+    <meta name="description" content="">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+    <meta name="apple-mobile-web-app-title" content="">
+    <meta name="application-name" content="">
+    <meta name="msapplication-TileColor" content="#7b1af7">
+    <meta name="format-detection" content="telephone=no">
+
+    <link rel="manifest" href="./manifest.json">
+
+</head>
+
+<body>
+
+</body>
+
+</html>
