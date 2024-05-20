a5fd829000a31dd6f7d608647930deeb73bee8d6
naxocaballero <naxocaballero@gmail.com>
2024-05-20 14:18:32 +0200

    update

---
+++ frontend/web/pages/keypad.php
@@ -0,0 +1,20 @@
+<section id="keypad">
+    <div class="container">
+        <label for="display">Cantidad</label>
+        <input type="text" id="display" placeholder="sats" readonly>
+        <div class="keypad">
+            <button class="key">1</button>
+            <button class="key">2</button>
+            <button class="key">3</button>
+            <button class="key">4</button>
+            <button class="key">5</button>
+            <button class="key">6</button>
+            <button class="key">7</button>
+            <button class="key">8</button>
+            <button class="key">9</button>
+            <button class="key">C</button>
+            <button class="key">0</button>
+            <button class="key">,</button>
+        </div>
+    </div>
+</section>
