ce035784025e76af6ce7665ebf65a942d76a6be5
Ana Gil <gilorozcoana@gmail.com>
2024-05-28 16:52:16 +0200

    Se devuelven solo las transacciones que estan pagadas

--- backend/main.go
+++ backend/main.go
@@ -85,7 +85,7 @@ func (c *Controller) getTransactions(ctx *gin.Context) {
 
 	var transactions []Transaction
 
-	if result := c.Database.Find(&transactions); result.Error != nil {
+	if result := c.Database.Find(&transactions).Where("status == PAGADO"); result.Error != nil {
 		log.Println("Error al obtener transacciones de la base de datos:", result.Error)
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error al obtener transacciones"})
 		return
