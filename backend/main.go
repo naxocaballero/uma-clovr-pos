d779846f5a0754cfd7f69c0f494125895d99570b
Ana Gil <gilorozcoana@gmail.com>
2024-05-30 01:21:56 +0200

    Configuración inicial

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"encoding/hex"
 	"log"
-	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/lightningnetwork/lnd/lnrpc"
-	"google.golang.org/grpc"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -22,25 +17,25 @@ type Controller struct {
 type TransactionStatus string
 
 const (
-	Pending TransactionStatus = "PENDIENTE"
-	Paid    TransactionStatus = "PAGADO"
-	Expired TransactionStatus = "EXPIRADO"
+	Pending   TransactionStatus = "PENDIENTE"
+	Paid      TransactionStatus = "PAGADO"
+	Expired   TransactionStatus = "EXPIRADO"
+	Refounded TransactionStatus = "DEVUELTA"
 )
 
 type Transaction struct {
 	gorm.Model
-	Amount       uint64            `json:"amount"`
-	InvoiceID    string            `json:"invoice_id"`
+	Amount       uint              `json:"amount"`
+	InvoiceID    uint              `json:"invoice_id"`
 	CreationDate time.Time         `json:"creation_date"`
 	Status       TransactionStatus `json:"status"`
-	Expiration   uint64            `json:"expiration"`
+	Expiration   uint64            `json:"timeout"`
+	RefoundID    uint              `json:"refound_id"`
+	Description  string            `json:"memo"`
 }
 
 var (
 	dbConnectionString = "host=localhost user=admin password=adminpw dbname=postgres port=5432 sslmode=disable TimeZone=Europe/Berlin"
-	uriVenta           = "TODO"
-	nodoVenta          lnrpc.LightningClient
-	conn               *grpc.ClientConn
 )
 
 func main() {
@@ -62,14 +57,6 @@ func main() {
 	if err != nil {
 		return
 	}
-
-	// Crear los clientes lnd
-	// Para el punto de venta
-	nodoVenta, conn, err = Conectar(uriVenta)
-	if err != nil {
-		log.Fatalf("Error %v creando el cliente del punto de venta", err)
-	}
-	defer conn.Close()
 }
 
 func (c *Controller) initDatabase() {
@@ -85,72 +72,16 @@ func (c *Controller) initDatabase() {
 	}
 }
 
-func (c *Controller) createInvoice(ctx *gin.Context) {
-	// Recibimos el valor de la invoice por un parámetro
-	// Opcionalmente, también es posible que recibamos el temporizador de la invoice a crear
-	var amount = ctx.Param("amount")
-	var expiration = ctx.Param("timeout")
-
-	// Creamos un objeto de la clase transaction inicialmente vacío
-	var newTransaction Transaction
-
-	if expiration == "" {
-		// No se ha recibido ningún timeout, por lo que utilizamos el valor por defecto
-		newTransaction.Expiration = 900
-	} else {
-		// Convertimos la cadena recibida en un float64
-		timeout, err := strconv.ParseUint(expiration, 10, 64)
-		if err != nil {
-			log.Println("Error convirtiendo el tiempo a uint64:", err)
-			return
-		}
-		// Lo asignamos
-		newTransaction.Expiration = timeout
-	}
-
-	// Repetimos la conversión para el parámetro amount
-	amountInt, err := strconv.ParseUint(amount, 10, 64)
-	if err != nil {
-		log.Println("Error convirtiendo la cantidad a float:", err)
-		return
-	}
-	newTransaction.Amount = amountInt
-
-	// Asignamos el tiempo de creación actual y estado pendiente de la transacción
-	newTransaction.Status = Pending
-	newTransaction.CreationDate = time.Now()
-
-	// Creamos la invoice utilizando el cliente
-	invoice := &lnrpc.Invoice{
-		Value: int64(amountInt),
-	}
-
-	resp, err := nodoVenta.AddInvoice(ctx, invoice)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, nil)
-	}
-	newTransaction.InvoiceID = hex.EncodeToString(resp.RHash)
-
-	c.Database.Create(&newTransaction)
-	ctx.JSON(http.StatusCreated, newTransaction)
+func (c *Controller) createInvoice(_ *gin.Context) {
+	log.Println("Solicitud para crear un invoice")
+	// Crear invoice
 }
 
 func (c *Controller) payInvoice(ctx *gin.Context) {
 	log.Println("Solicitud para pagar un invoice")
-	// Pagar invoice
 }
 
-func (c *Controller) getTransactions(ctx *gin.Context) {
-	log.Println("Solicitud para obtener todas las transacciones")
-
-	var transactions []Transaction
-
-	if result := c.Database.Find(&transactions).Where("status == PAGADO"); result.Error != nil {
-		log.Println("Error al obtener transacciones de la base de datos:", result.Error)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error al obtener transacciones"})
-		return
-	}
-
-	// Devuelve la lista de transacciones como una respuesta JSON
-	ctx.IndentedJSON(http.StatusOK, transactions)
+func (c *Controller) getTransactions(_ *gin.Context) {
+	log.Println("Solicitud para obtener las transacciones")
+	// Obtener transacciones de la BD
 }
---
+++ backend/payment.go
@@ -0,0 +1,60 @@
+// payment.go
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/lightningnetwork/lnd/lnrpc"
+)
+
+func PagarInvoice(uri string, paymentRequest string, amount uint) error {
+
+	//Establecer conexion con el nodo
+	nodeClient, nodeConn, err := Conectar(uri)
+	if err != nil {
+		log.Println("Failed to connect with the node: ", err)
+		return err
+	}
+
+	defer func() {
+		err := nodeConn.Close()
+		if err != nil {
+			log.Println("Error closing the connection:", err)
+		}
+	}()
+
+	//Decodificar el paymentRequest
+	decodeReq := &lnrpc.PayReqString{PayReq: paymentRequest}
+	decodeResp, err := nodeClient.DecodePayReq(context.Background(), decodeReq)
+	if err != nil {
+		log.Println("Error leyendo el paymentRequest: ", err)
+		return err
+	}
+
+	//Verificar que el amount en el paymentRequest es igual al amount pasado
+	if decodeResp.NumSatoshis != amount {
+		errMsg := fmt.Sprintf("La cantidad de satoshis no coincide con la transaccion original")
+		log.Println(errMsg)
+		return fmt.Errorf(errMsg)
+	}
+
+	// Pagar invoice
+	payment := &lnrpc.SendRequest{
+		PaymentRequest: paymentRequest,
+	}
+
+	response, err := nodeClient.SendPaymentSync(context.Background(), payment)
+	if err != nil {
+		return err
+	}
+
+	if response.PaymentError != "" {
+		return fmt.Errorf("error en el pago: %s", response.PaymentError)
+	}
+
+	fmt.Println("Successful payment")
+
+	return nil
+}
